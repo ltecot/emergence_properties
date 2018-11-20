c6cad91264cad5feab0f2674942845f641fe1d89
Lucas Tecot <lucastecot@gmail.com>
2018-11-20 15:36:41 -0800

    Rough equil prop

--- models/equilibrium_propagation_baseline.py
+++ models/equilibrium_propagation_baseline.py
@@ -1,16 +1,5 @@
 # import cPickle
-# from external_world import External_World
-# import numpy as np
-# import os
-# import theano
-# import theano.tensor as T
-
-# import argparse
-# import gym
 import numpy as np
-# from itertools import count
-# from collections import namedtuple
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -20,206 +9,74 @@ from torch.distributions import Categorical
 def rho(s):
     return torch.clamp(s,0.,1.)
 
-class Network(nn.Module):
-    def __init__(self):
-        super(Network, self).__init__()
-                self.path = name+".save"
+# MLP Model. Might want to do a vanilla RNN later.
+class Equilibrium_Propegation_Network(nn.Module):
+    def __init__(self, input_size, output_size, epsilon, alpha, learning_rate, name="equil_prop_baseline"):
+        super(Equilibrium_Propegation_Network, self).__init__()
 
-        # LOAD/INITIALIZE PARAMETERS
-        self.biases, self.weights, self.hyperparameters, self.training_curves = self.__load_params(hyperparameters)
-
-        # LOAD EXTERNAL WORLD (=DATA)
-        # self.external_world = External_World()
+        self.path = name+".save"
+        self.hyperparameters = {}
+        self.hyperparameters["hidden_sizes"] = 128
+        # self.hyperparameters["batch_size"] = 32
+        self.hyperparameters["learning_rate"] = learning_rate
         
-        # INITIALIZE PERSISTENT PARTICLES
-        # dataset_size = self.external_world.size_dataset
-        layer_sizes = [28*28] + self.hyperparameters["hidden_sizes"] + [10]
-        values = [np.zeros((dataset_size, layer_size), dtype=theano.config.floatX) for layer_size in layer_sizes[1:]]
-        self.persistent_particles  = [theano.shared(value, borrow=True) for value in values]
+        self.input = torch.zeros([input_size], dtype=torch.float32)
+        self.hidden = torch.zeros([self.hyperparameters["hidden_sizes"]], dtype=torch.float32)
+        self.output = torch.zeros([output_size], dtype=torch.float32)
+        self.units = [self.input, self.hidden, self.output]
+        self.free_units = [self.hidden, self.output]
+        
+        self.biases = [torch.zeros(t.size) for t in self.units]
+        self.weights = [nn.init.xavier_uniform_(torch.zeros(t.size)) for pre_t, post_t in zip(self.units[:-1],self.units[1:]) ]
+        self.params = [self.biases, self.weights]
 
-        # LAYERS = MINI-BACTHES OF DATA + MINI-BACTHES OF PERSISTENT PARTICLES
-        batch_size = self.hyperparameters["batch_size"]
-        self.index = theano.shared(np.int32(0), name='index') # index of a mini-batch
-
-        self.x_data = self.external_world.x[self.index * batch_size: (self.index + 1) * batch_size]
-        self.y_data = self.external_world.y[self.index * batch_size: (self.index + 1) * batch_size]
-        self.y_data_one_hot = T.extra_ops.to_one_hot(self.y_data, 10)
-
-        self.layers = [self.x_data]+[particle[self.index * batch_size: (self.index + 1) * batch_size] for particle in self.persistent_particles]
-
-        # BUILD THEANO FUNCTIONS
-        self.change_mini_batch_index = self.__build_change_mini_batch_index()
-        self.measure                 = self.__build_measure()
-        self.free_phase              = self.__build_free_phase()
-        self.weakly_clamped_phase    = self.__build_weakly_clamped_phase()
-
-    def forward(self, x):
-        # x = F.relu(self.affine1(x))
-        # action_scores = self.action_head(x)
-        # state_values = self.value_head(x)
-        # return F.softmax(action_scores, dim=-1), state_values
-
-class Network(object):
-
-    def __init__(self, name, hyperparameters=dict()):
-
-
-
-    def save_params(self):
-        f = file(self.path, 'wb')
-        biases_values  = [b.get_value() for b in self.biases]
-        weights_values = [W.get_value() for W in self.weights]
-        to_dump        = biases_values, weights_values, self.hyperparameters, self.training_curves
-        cPickle.dump(to_dump, f, protocol=cPickle.HIGHEST_PROTOCOL)
-        f.close()
-
-    def __load_params(self, hyperparameters):
-
-        hyper = hyperparameters
-
-        # Glorot/Bengio weight initialization
-        def initialize_layer(n_in, n_out):
-            rng = np.random.RandomState()
-            W_values = np.asarray(
-                rng.uniform(
-                    low=-np.sqrt(6. / (n_in + n_out)),
-                    high=np.sqrt(6. / (n_in + n_out)),
-                    size=(n_in, n_out)
-                ),
-                dtype=theano.config.floatX
-            )
-            return W_values
-
-        if os.path.isfile(self.path):
-            f = file(self.path, 'rb')
-            biases_values, weights_values, hyperparameters, training_curves = cPickle.load(f)
-            f.close()
-            for k,v in hyper.iteritems():
-                hyperparameters[k]=v
-        else:
-            layer_sizes = [28*28] + hyperparameters["hidden_sizes"] + [10]
-            biases_values  = [np.zeros((size,), dtype=theano.config.floatX) for size in layer_sizes]
-            weights_values = [initialize_layer(size_pre,size_post) for size_pre,size_post in zip(layer_sizes[:-1],layer_sizes[1:])]
-            training_curves = dict()
-            training_curves["training error"]   = list()
-            training_curves["validation error"] = list()
-
-        biases  = [theano.shared(value=value, borrow=True) for value in biases_values]
-        weights = [theano.shared(value=value, borrow=True) for value in weights_values]
-
-        return biases, weights, hyperparameters, training_curves
-
-    # SET INDEX OF THE MINI BATCH
-    def __build_change_mini_batch_index(self):
-
-        index_new = T.iscalar("index_new")
-
-        change_mini_batch_index = theano.function(
-            inputs=[index_new],
-            outputs=[],
-            updates=[(self.index,index_new)]
-        )
-
-        return change_mini_batch_index
+        self.energy_optimizer = optim.Adam(self.free_units, lr=epsilon)
+        self.model_optimizer = optim.Adam(self.params, lr=alpha)
 
     # ENERGY FUNCTION, DENOTED BY E
-    def __energy(self, layers):
-        squared_norm    =   sum( [T.batched_dot(rho(layer),rho(layer))       for layer      in layers] ) / 2.
-        linear_terms    = - sum( [T.dot(rho(layer),b)                        for layer,b    in zip(layers,self.biases)] )
-        quadratic_terms = - sum( [T.batched_dot(T.dot(rho(pre),W),rho(post)) for pre,W,post in zip(layers[:-1],self.weights,layers[1:])] )
+    def __energy(self):
+        squared_norm = torch.sum([torch.mm(rho(layer),rho(layer)) for layer in self.units]) / 2.
+        linear_terms = -torch.sum([torch.mm(rho(layer),b) for layer,b in zip(self.units,self.biases)])
+        quadratic_terms = -torch.sum([torch.mm(torch.mm(rho(pre),W),rho(post)) for pre,W,post in zip(self.units[:-1],self.weights,self.units[1:])])
         return squared_norm + linear_terms + quadratic_terms
 
     # COST FUNCTION, DENOTED BY C
-    def __cost(self, layers):
-        return ((layers[-1] - self.y_data_one_hot) ** 2).sum(axis=1)
+    def __cost(self, ground_truth):
+        return ((self.units[-1] - ground_truth) ** 2).sum(axis=1)
 
     # TOTAL ENERGY FUNCTION, DENOTED BY F
-    def __total_energy(self, layers, beta):
-        return self.__energy(layers) + beta * self.__cost(layers)
+    def __total_energy(self, beta, ground_truth):
+        if beta == 0:
+            return self.__energy()
+        else:
+            return self.__energy() + beta * self.__cost(ground_truth)
 
     # MEASURES THE ENERGY, THE COST AND THE MISCLASSIFICATION ERROR FOR THE CURRENT STATE OF THE NETWORK
-    def __build_measure(self):
+    def predict_and_measure(self, ground_truth):
+        E = torch.mean(self.__energy())
+        C = torch.mean(self.__cost(ground_truth))
+        y_prediction = torch.argmax(self.layers[-1], axis=1)
+        return y_prediction, E, C
 
-        E = T.mean(self.__energy(self.layers))
-        C = T.mean(self.__cost(self.layers))
-        y_prediction = T.argmax(self.layers[-1], axis=1)
-        error        = T.mean(T.neq(y_prediction, self.y_data))
+    # Coverges network towards fixed point.
+    def forward(self, input, n_iterations, beta=0, ground_truth=None):
+        self.input = input
+        for _ in n_iterations:
+            self.energy_optimizer.zero_grad()
+            energy = torch.stack(self.__total_energy(beta, ground_truth)).sum()
+            energy.backward()
+            self.energy_optimizer.step()
+            for layer in self.free_units:
+                torch.clamp(layer,0.,1.)
+        return self.output
 
-        measure = theano.function(
-            inputs=[],
-            outputs=[E, C, error]
-        )
-
-        return measure
-
-    def __build_free_phase(self):
-
-        n_iterations = T.iscalar('n_iterations')
-        epsilon  = T.fscalar('epsilon')
-
-        def step(*layers):
-            E_sum = T.sum(self.__energy(layers))
-            layers_dot = T.grad(-E_sum, list(layers)) # temporal derivative of the state (free trajectory)
-            layers_new = [layers[0]]+[T.clip(layer+epsilon*dot,0.,1.) for layer,dot in zip(layers,layers_dot)][1:]
-            return layers_new
-
-        ( layers, updates ) = theano.scan(
-            step,
-            outputs_info=self.layers,
-            n_steps=n_iterations
-        )
-        layers_end = [layer[-1] for layer in layers]
-
-        for particles,layer,layer_end in zip(self.persistent_particles,self.layers[1:],layers_end[1:]):
-            updates[particles] = T.set_subtensor(layer,layer_end)
-
-        free_phase = theano.function(
-            inputs=[n_iterations,epsilon],
-            outputs=[],
-            updates=updates
-        )
-
-        return free_phase
-
-    def __build_weakly_clamped_phase(self):
-
-        n_iterations = T.iscalar('n_iterations')
-        epsilon  = T.fscalar('epsilon')
-        beta = T.fscalar('beta')
-        alphas = [T.fscalar("alpha_W"+str(r+1)) for r in range(len(self.weights))]
-
-        def step(*layers):
-            F_sum = T.sum(self.__total_energy(layers, beta))
-            layers_dot = T.grad(-F_sum, list(layers)) # temporal derivative of the state (weakly clamped trajectory)
-            layers_new = [layers[0]]+[T.clip(layer+epsilon*dot,0.,1.) for layer,dot in zip(layers,layers_dot)][1:]
-            return layers_new
-
-        ( layers, updates ) = theano.scan(
-            step,
-            outputs_info=self.layers,
-            n_steps=n_iterations
-        )
-        layers_weakly_clamped = [layer[-1] for layer in layers]
-
-        E_mean_free           = T.mean(self.__energy(self.layers))
-        E_mean_weakly_clamped = T.mean(self.__energy(layers_weakly_clamped))
-        biases_dot            = T.grad( (E_mean_weakly_clamped-E_mean_free) / beta, self.biases,  consider_constant=layers_weakly_clamped)
-        weights_dot           = T.grad( (E_mean_weakly_clamped-E_mean_free) / beta, self.weights, consider_constant=layers_weakly_clamped)
-
-        biases_new  = [b - alpha * dot for b,alpha,dot in zip(self.biases[1:],alphas,biases_dot[1:])]
-        weights_new = [W - alpha * dot for W,alpha,dot in zip(self.weights,   alphas,weights_dot)]
-        
-        Delta_log = [T.sqrt( ((W_new - W) ** 2).mean() ) / T.sqrt( (W ** 2).mean() ) for W,W_new in zip(self.weights,weights_new)]
-        
-        for bias, bias_new in zip(self.biases[1:],biases_new):
-            updates[bias]=bias_new
-        for weight, weight_new in zip(self.weights,weights_new):
-            updates[weight]=weight_new
-
-        weakly_clamped_phase = theano.function(
-            inputs=[n_iterations, epsilon, beta]+alphas,
-            outputs=Delta_log,
-            updates=updates
-        )
-
-        return weakly_clamped_phase
+    # Does contrastive parameter optimization. Change to Hebbian later.
+    def optimize(self, input, n_iterations, reset_states=False, beta=0, ground_truth=None):
+        self.forward(input, n_iterations, beta=0, ground_truth=None)  # Free Phase
+        free_energy = self.__total_energy(0, ground_truth)
+        self.forward(input, n_iterations, beta=beta, ground_truth=ground_truth)  # Constrained Phase
+        constrained_energy = self.__total_energy(beta, ground_truth)
+        self.model_optimizer.zero_grad()
+        param_loss = torch.stack((constrained_energy - free_energy) / beta).sum()
+        param_loss.backward()
+        self.model_optimizer.step()
--- sandbox/mnist_example.py
+++ sandbox/mnist_example.py
@@ -6,7 +6,23 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 
-from models.equilibrium_propagation_baseline import Equilibrium_Propegation_Network
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
 
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
@@ -41,25 +57,25 @@ def test(args, model, device, test_loader):
 
 def main():
     # Training settings
-    # parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    # parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-    #                     help='input batch size for training (default: 64)')
-    # parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-    #                     help='input batch size for testing (default: 1000)')
-    # parser.add_argument('--epochs', type=int, default=10, metavar='N',
-    #                     help='number of epochs to train (default: 10)')
-    # parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
-    #                     help='learning rate (default: 0.01)')
-    # parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
-    #                     help='SGD momentum (default: 0.5)')
-    # parser.add_argument('--no-cuda', action='store_true', default=False,
-    #                     help='disables CUDA training')
-    # parser.add_argument('--seed', type=int, default=1, metavar='S',
-    #                     help='random seed (default: 1)')
-    # parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-    #                     help='how many batches to wait before logging training status')
-    # args = parser.parse_args()
-    # use_cuda = not args.no_cuda and torch.cuda.is_available()
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
+                        help='input batch size for training (default: 64)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                        help='learning rate (default: 0.01)')
+    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                        help='SGD momentum (default: 0.5)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                        help='how many batches to wait before logging training status')
+    args = parser.parse_args()
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
 
     torch.manual_seed(args.seed)
 
@@ -81,10 +97,8 @@ def main():
         batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
 
-    # model = Net().to(device)
-    # optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
-
-    model = Equilibrium_Propegation_Network()
+    model = Net().to(device)
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
     for epoch in range(1, args.epochs + 1):
         train(args, model, device, train_loader, optimizer, epoch)
