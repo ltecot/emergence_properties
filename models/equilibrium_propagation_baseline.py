258bebd212bba546d151367c1a28b04647be0776
Lucas Tecot <lucastecot@gmail.com>
2018-11-20 18:48:09 -0800

    draft needs debug

--- models/equilibrium_propagation_baseline.py
+++ models/equilibrium_propagation_baseline.py
@@ -11,14 +11,14 @@ def rho(s):
 
 # MLP Model. Might want to do a vanilla RNN later.
 class Equilibrium_Propegation_Network(nn.Module):
-    def __init__(self, input_size, output_size, epsilon, alpha, learning_rate, name="equil_prop_baseline"):
+    def __init__(self, input_size, output_size, epsilon, alpha, name="equil_prop_baseline"):
         super(Equilibrium_Propegation_Network, self).__init__()
 
         self.path = name+".save"
         self.hyperparameters = {}
         self.hyperparameters["hidden_sizes"] = 128
         # self.hyperparameters["batch_size"] = 32
-        self.hyperparameters["learning_rate"] = learning_rate
+        # self.hyperparameters["learning_rate"] = learning_rate
         
         self.input = torch.zeros([input_size], dtype=torch.float32)
         self.hidden = torch.zeros([self.hyperparameters["hidden_sizes"]], dtype=torch.float32)
@@ -52,11 +52,11 @@ class Equilibrium_Propegation_Network(nn.Module):
             return self.__energy() + beta * self.__cost(ground_truth)
 
     # MEASURES THE ENERGY, THE COST AND THE MISCLASSIFICATION ERROR FOR THE CURRENT STATE OF THE NETWORK
-    def predict_and_measure(self, ground_truth):
+    def __predict_and_measure(self, ground_truth):
         E = torch.mean(self.__energy())
         C = torch.mean(self.__cost(ground_truth))
-        y_prediction = torch.argmax(self.layers[-1], axis=1)
-        return y_prediction, E, C
+        y_softmax = F.log_softmax(self.output, dim=1)
+        return y_softmax, E, C
 
     # Coverges network towards fixed point.
     def forward(self, input, n_iterations, beta=0, ground_truth=None):
@@ -68,15 +68,17 @@ class Equilibrium_Propegation_Network(nn.Module):
             self.energy_optimizer.step()
             for layer in self.free_units:
                 torch.clamp(layer,0.,1.)
-        return self.output
+        return F.log_softmax(self.output, dim=1)
 
     # Does contrastive parameter optimization. Change to Hebbian later.
-    def optimize(self, input, n_iterations, reset_states=False, beta=0, ground_truth=None):
+    def optimize(self, input, n_iterations, beta, ground_truth):
         self.forward(input, n_iterations, beta=0, ground_truth=None)  # Free Phase
         free_energy = self.__total_energy(0, ground_truth)
+        y_pred, mean_free_energy, mean_free_cost = self.__predict_and_measure(ground_truth)
         self.forward(input, n_iterations, beta=beta, ground_truth=ground_truth)  # Constrained Phase
         constrained_energy = self.__total_energy(beta, ground_truth)
         self.model_optimizer.zero_grad()
         param_loss = torch.stack((constrained_energy - free_energy) / beta).sum()
         param_loss.backward()
         self.model_optimizer.step()
+        return y_pred, mean_free_energy, mean_free_cost
