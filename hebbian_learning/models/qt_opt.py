4893382e228e796392bf9b7e2e4d60d2b3b17013
Lucas Tecot <lucastecot@gmail.com>
2018-12-01 15:47:14 -0800

    qt_opt refactor

--- hebbian_learning/models/qt_opt.py
+++ hebbian_learning/models/qt_opt.py
@@ -6,149 +6,140 @@ import gym
 from torch.distributions import Categorical
 import random
 
+class MLP(nn.Module):
+    def __init__(self, n_in, n_out, hidden, learning_rate):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(n_in, hidden)
+        self.fc1.weight.data.normal_(0, 0.01)   # initialization
+        self.out = nn.Linear(hidden, n_out)
+        self.out.weight.data.normal_(0, 0.01)   # initialization
+
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate)
+        self.loss_func = nn.MSELoss()
+
+        self.n_in = n_in
+        self.n_out = n_out
+        self.hidden = hidden
+        self.learning_rate = learning_rate
+
+    def forward(self, input):
+        # x = self.fc1(torch.cat((states.float(), actions.float()), dim=1))
+        x = self.fc1(input)
+        x = F.relu(x)
+        y = self.out(x)
+        return y
+        # return F.softmax(actions_value)
+
+    def optimize(self, pred, target):
+        loss = self.loss_func(pred, target)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+    def copy(self):
+        new_net = MLP(self.n_in, self.n_out, self.hidden, self.learning_rate)
+        new_net.load_state_dict(self.state_dict())
+        return new_net
+
+    # In place copy
+    def copy_(self, source_net):
+        self.load_state_dict(source_net.state_dict())
+
+class Qt_Opt(object):
+    def __init__(self, network, num_states, num_actions, memory_capacity, batch_size, target_replace_period, epsilon, gamma):
+        self.eval_net = network
+        self.target_net = network.copy()
+
+        self.learn_step_counter = 0                                     # for target updating
+        self.memory_counter = 0                                         # for storing memory
+        self.memory = np.zeros((memory_capacity, num_states * 2 + num_actions + 2))     # initialize memory
+        
+        self.num_states = num_states
+        self.num_actions = num_actions
+        self.memory_capacity = memory_capacity
+        self.batch_size = batch_size
+        self.target_replace_period = target_replace_period
+        self.eps = epsilon
+        self.gamma = gamma
+
+    def choose_action(self, x):
+        state = torch.from_numpy(x).float().view(-1)
+        if np.random.uniform() < self.eps:
+            v0 = self.eval_net.forward(torch.cat((state.view(1, -1), torch.tensor([[0]]).float()), dim=1))
+            v1 = self.eval_net.forward(torch.cat((state.view(1, -1), torch.tensor([[1]]).float()), dim=1))
+            action = 1 if v1 > v0 else 0
+        else:
+            action = 1 if np.random.uniform() > 0.5 else 0
+        return action
+
+    def store_transition(self, s, a, r, done, s_):
+        transition = np.hstack((s, [a, r, done], s_))
+        index = self.memory_counter % self.memory_capacity
+        self.memory[index, :] = transition
+        self.memory_counter += 1
+
+    def target_max(self, b_s_):
+        v0 = self.target_net.forward(torch.cat((b_s_, torch.zeros(b_s_.shape[0], 1).float()), dim=1))
+        v1 = self.target_net.forward(torch.cat((b_s_, torch.ones(b_s_.shape[0], 1).float()), dim=1))
+        return torch.max(v0, v1)
+
+    def learn(self):
+        if self.memory_counter > self.memory_capacity:
+            # target parameter update
+            if self.learn_step_counter % self.target_replace_period == 0:
+                # self.target_net.load_state_dict(self.eval_net.state_dict())
+                self.target_net.copy_(self.eval_net)
+            self.learn_step_counter += 1
+            sample_index = np.random.choice(self.memory_capacity, self.batch_size )
+            b_memory = self.memory[sample_index, :]
+            b_s = torch.FloatTensor(b_memory[:, :self.num_states])
+            b_a = torch.LongTensor(b_memory[:, self.num_states:self.num_states+self.num_actions])
+            b_r = torch.FloatTensor(b_memory[:, self.num_states+self.num_actions:self.num_states+self.num_actions+1])
+            b_t = torch.FloatTensor(b_memory[:, self.num_states+self.num_actions+1:self.num_states+self.num_actions+2])
+            b_s_ = torch.FloatTensor(b_memory[:, -self.num_states:])
+            q_eval = self.eval_net(torch.cat((b_s.float(), b_a.float()), dim=1))
+            q_next = self.target_max(b_s_).detach()     # detach from graph, don't backpropagate
+            q_target = b_r + self.gamma * (1 - b_t) * q_next   # shape (batch, 1)
+            self.eval_net.optimize(q_eval, q_target)
+            # loss = self.loss_func(q_eval, q_target)
+            # Run optimizer
+            # self.optimizer.zero_grad()
+            # loss.backward()
+            # self.optimizer.step()
+
 # Hyper Parameters
 env = gym.make('CartPole-v0')
-LR = 0.001
+LR = 0.01
 GAMMA = 0.99  
-EPSILON = 0.9
-# EPSILON_INIT = 0.1
-# EPSILON_FIN = 0.99 
-# EPSILON_RATE = 100000
+EPSILON = 0.95
 BATCH_SIZE = 16
 TARGET_REPLACE_ITER = 10
 MEMORY_CAPACITY = 256
-
-
-# env = gym.make('MountainCar-v0')
-# LR = 0.01
-# GAMMA = 0.99  
-# EPSILON_INIT = 0.1
-# EPSILON_FIN = 0.99 
-# EPSILON_RATE = 100000
-# BATCH_SIZE = 256
-# TARGET_REPLACE_ITER = 100 
-# MEMORY_CAPACITY = 256
-
-# env = gym.make('Acrobot-v1')
-# env = gym.make('MountainCarContinuous-v0')
-# env = gym.make('Pendulum-v0')
 
 env.seed(1337)
 torch.manual_seed(1337)
 env = env.unwrapped
 N_ACTIONS = 1 # env.action_space.n
 N_STATES = env.observation_space.shape[0]
-# N_STATES = 1
 
-class Net(nn.Module):
-    def __init__(self, ):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(N_STATES + N_ACTIONS, 64)
-        self.fc1.weight.data.normal_(0, 0.01)   # initialization
-        self.out = nn.Linear(64, 1)
-        self.out.weight.data.normal_(0, 0.01)   # initialization
+network = MLP(N_STATES + N_ACTIONS, 1, 64, LR)
+rl_model = Qt_Opt(network, N_STATES, N_ACTIONS, MEMORY_CAPACITY, TARGET_REPLACE_ITER, BATCH_SIZE, EPSILON, GAMMA)
 
-    def forward(self, states, actions):
-        x = self.fc1(torch.cat((states.float(), actions.float()), dim=1))
-        x = F.relu(x)
-        value = self.out(x)
-        return value
-        # return F.softmax(actions_value)
-
-class DQN(object):
-    def __init__(self):
-        self.eval_net, self.target_net = Net(), Net()
-
-        self.learn_step_counter = 0                                     # for target updating
-        self.memory_counter = 0                                         # for storing memory
-        self.memory = np.zeros((MEMORY_CAPACITY, N_STATES * 2 + 3))     # initialize memory
-        self.optimizer = torch.optim.Adam(self.eval_net.parameters(), lr=LR)
-        self.loss_func = nn.MSELoss()
-        self.eps = EPSILON
-        # self.time_count = 1
-
-    def choose_action(self, x):
-        state = torch.from_numpy(x).float().view(-1)
-        # probs = self.eval_net.forward(state)
-        # eps_progress = self.time_count / EPSILON_RATE if self.time_count / EPSILON_RATE < 1 else 1
-        # self.eps = EPSILON_INIT * (1 - eps_progress) + EPSILON_FIN * eps_progress
-        # self.time_count += 1
-        if np.random.uniform() < EPSILON:
-            v0 = self.eval_net.forward(state.view(1, -1), torch.tensor([[0]]).float())
-            v1 = self.eval_net.forward(state.view(1, -1), torch.tensor([[1]]).float())
-            action = 1 if v1 > v0 else 0
-            # m = Categorical(probs)
-            # action = m.sample()
-            # action = action.item()
-        else:
-            action = env.action_space.sample()
-        return action
-
-    def store_transition(self, s, a, r, done, s_):
-        transition = np.hstack((s, [a, r, done], s_))
-        # replace the old memory with new memory
-        index = self.memory_counter % MEMORY_CAPACITY
-        self.memory[index, :] = transition
-        self.memory_counter += 1
-
-    def target_max(self, b_s_):
-        v0 = self.target_net.forward(b_s_, torch.zeros(b_s_.shape[0], 1).float())
-        v1 = self.target_net.forward(b_s_, torch.ones(b_s_.shape[0], 1).float())
-        return torch.max(v0, v1)
-
-    def learn(self):
-        # target parameter update
-        if self.learn_step_counter % TARGET_REPLACE_ITER == 0:
-            self.target_net.load_state_dict(self.eval_net.state_dict())
-        self.learn_step_counter += 1
-        # sample batch transitions
-        sample_index = np.random.choice(MEMORY_CAPACITY, BATCH_SIZE)
-        b_memory = self.memory[sample_index, :]
-        b_s = torch.FloatTensor(b_memory[:, :N_STATES])
-        b_a = torch.LongTensor(b_memory[:, N_STATES:N_STATES+1].astype(int))
-        b_r = torch.FloatTensor(b_memory[:, N_STATES+1:N_STATES+2])
-        b_t = torch.FloatTensor(b_memory[:, N_STATES+2:N_STATES+3])
-        b_s_ = torch.FloatTensor(b_memory[:, -N_STATES:])
-        # q_eval w.r.t the action in experience
-        # q_eval = self.eval_net(b_s, b_a).gather(1, b_s, b_a)  # shape (batch, 1)
-        # q_next = self.target_net(b_s_).detach()     # detach from graph, don't backpropagate
-        # q_target = b_r + GAMMA * q_next.max(1)[0].view(BATCH_SIZE, 1)   # shape (batch, 1)
-        # loss = self.loss_func(q_eval, q_target)
-        q_eval = self.eval_net(b_s, b_a)
-        q_next = self.target_max(b_s_).detach()     # detach from graph, don't backpropagate
-        q_target = b_r + GAMMA * (1 - b_t) * q_next   # shape (batch, 1)
-        loss = self.loss_func(q_eval, q_target)
-        # Run optimizer
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
-
-dqn = DQN()
-
-running_reward = 0
-# training_done = False
+running_reward = 20
 for i_episode in range(100000):
-    # if training_done:
-    #     break
     s = env.reset()
     ep_r = 0
     for t in range(100000):
         env.render()
-        a = dqn.choose_action(s)
+        a = rl_model.choose_action(s)
         s_, r, done, info = env.step(a)
-        dqn.store_transition(s, a, r, done, s_)
+        rl_model.store_transition(s, a, r, done, s_)
         s = s_
         ep_r += r
-        if dqn.memory_counter > MEMORY_CAPACITY:
-            dqn.learn()
+        rl_model.learn()
         if done:
             running_reward = running_reward * 0.99 + ep_r * 0.01
             print('Episode {}\treward: {:.2f}\tAverage reward: {:.2f}'.format(
                 i_episode, ep_r, running_reward))
             break
-            # if running_reward > env.spec.reward_threshold:
-            #     print("Solved! Running reward is now {} and "
-            #           "the last episode got {} reward!".format(running_reward, ep_r))
-            #     training_done = True
-            # break
 env.close()
--- sandbox/qt_opt.py
+++ sandbox/qt_opt.py
@@ -10,12 +10,12 @@ import random
 env = gym.make('CartPole-v0')
 LR = 0.001
 GAMMA = 0.99  
-EPSILON = 0.95
+EPSILON = 0.9
 # EPSILON_INIT = 0.1
 # EPSILON_FIN = 0.99 
-EPSILON_RATE = 100000
-BATCH_SIZE = 256
-TARGET_REPLACE_ITER = 100 
+# EPSILON_RATE = 100000
+BATCH_SIZE = 16
+TARGET_REPLACE_ITER = 10
 MEMORY_CAPACITY = 256
 
 
@@ -33,22 +33,23 @@ MEMORY_CAPACITY = 256
 # env = gym.make('MountainCarContinuous-v0')
 # env = gym.make('Pendulum-v0')
 
-gym.spaces.prng.seed(1337)
+env.seed(1337)
+torch.manual_seed(1337)
 env = env.unwrapped
-N_ACTIONS = env.action_space.n
-# N_STATES = env.observation_space.shape[0]
-N_STATES = 1
+N_ACTIONS = 1 # env.action_space.n
+N_STATES = env.observation_space.shape[0]
+# N_STATES = 1
 
 class Net(nn.Module):
     def __init__(self, ):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(N_STATES + N_ACTIONS, 4)
-        # self.fc1.weight.data.normal_(0, 0.1)   # initialization
-        self.out = nn.Linear(4, 1)
-        # self.out.weight.data.normal_(0, 0.1)   # initialization
+        self.fc1 = nn.Linear(N_STATES + N_ACTIONS, 64)
+        self.fc1.weight.data.normal_(0, 0.01)   # initialization
+        self.out = nn.Linear(64, 1)
+        self.out.weight.data.normal_(0, 0.01)   # initialization
 
     def forward(self, states, actions):
-        x = self.fc1(states + actions)
+        x = self.fc1(torch.cat((states.float(), actions.float()), dim=1))
         x = F.relu(x)
         value = self.out(x)
         return value
@@ -60,23 +61,21 @@ class DQN(object):
 
         self.learn_step_counter = 0                                     # for target updating
         self.memory_counter = 0                                         # for storing memory
-        self.memory = np.zeros((MEMORY_CAPACITY, N_STATES * 2 + 2))     # initialize memory
+        self.memory = np.zeros((MEMORY_CAPACITY, N_STATES * 2 + 3))     # initialize memory
         self.optimizer = torch.optim.Adam(self.eval_net.parameters(), lr=LR)
         self.loss_func = nn.MSELoss()
-        self.eps = EPSILON_INIT
+        self.eps = EPSILON
         # self.time_count = 1
 
-    def 
-
     def choose_action(self, x):
-        state = torch.from_numpy(x).float().unsqueeze(0)
+        state = torch.from_numpy(x).float().view(-1)
         # probs = self.eval_net.forward(state)
         # eps_progress = self.time_count / EPSILON_RATE if self.time_count / EPSILON_RATE < 1 else 1
         # self.eps = EPSILON_INIT * (1 - eps_progress) + EPSILON_FIN * eps_progress
         # self.time_count += 1
         if np.random.uniform() < EPSILON:
-            v0 = self.eval_net.forward(state, [0])
-            v1 = self.eval_net.forward(state, [1])
+            v0 = self.eval_net.forward(state.view(1, -1), torch.tensor([[0]]).float())
+            v1 = self.eval_net.forward(state.view(1, -1), torch.tensor([[1]]).float())
             action = 1 if v1 > v0 else 0
             # m = Categorical(probs)
             # action = m.sample()
@@ -85,12 +84,17 @@ class DQN(object):
             action = env.action_space.sample()
         return action
 
-    def store_transition(self, s, a, r, s_):
-        transition = np.hstack((s, [a, r], s_))
+    def store_transition(self, s, a, r, done, s_):
+        transition = np.hstack((s, [a, r, done], s_))
         # replace the old memory with new memory
         index = self.memory_counter % MEMORY_CAPACITY
         self.memory[index, :] = transition
         self.memory_counter += 1
+
+    def target_max(self, b_s_):
+        v0 = self.target_net.forward(b_s_, torch.zeros(b_s_.shape[0], 1).float())
+        v1 = self.target_net.forward(b_s_, torch.ones(b_s_.shape[0], 1).float())
+        return torch.max(v0, v1)
 
     def learn(self):
         # target parameter update
@@ -103,11 +107,16 @@ class DQN(object):
         b_s = torch.FloatTensor(b_memory[:, :N_STATES])
         b_a = torch.LongTensor(b_memory[:, N_STATES:N_STATES+1].astype(int))
         b_r = torch.FloatTensor(b_memory[:, N_STATES+1:N_STATES+2])
+        b_t = torch.FloatTensor(b_memory[:, N_STATES+2:N_STATES+3])
         b_s_ = torch.FloatTensor(b_memory[:, -N_STATES:])
         # q_eval w.r.t the action in experience
-        q_eval = self.eval_net(b_s).gather(1, b_a)  # shape (batch, 1)
-        q_next = self.target_net(b_s_).detach()     # detach from graph, don't backpropagate
-        q_target = b_r + GAMMA * q_next.max(1)[0].view(BATCH_SIZE, 1)   # shape (batch, 1)
+        # q_eval = self.eval_net(b_s, b_a).gather(1, b_s, b_a)  # shape (batch, 1)
+        # q_next = self.target_net(b_s_).detach()     # detach from graph, don't backpropagate
+        # q_target = b_r + GAMMA * q_next.max(1)[0].view(BATCH_SIZE, 1)   # shape (batch, 1)
+        # loss = self.loss_func(q_eval, q_target)
+        q_eval = self.eval_net(b_s, b_a)
+        q_next = self.target_max(b_s_).detach()     # detach from graph, don't backpropagate
+        q_target = b_r + GAMMA * (1 - b_t) * q_next   # shape (batch, 1)
         loss = self.loss_func(q_eval, q_target)
         # Run optimizer
         self.optimizer.zero_grad()
@@ -127,7 +136,7 @@ for i_episode in range(100000):
         env.render()
         a = dqn.choose_action(s)
         s_, r, done, info = env.step(a)
-        dqn.store_transition(s, a, r, s_)
+        dqn.store_transition(s, a, r, done, s_)
         s = s_
         ep_r += r
         if dqn.memory_counter > MEMORY_CAPACITY:
@@ -143,4 +152,3 @@ for i_episode in range(100000):
             #     training_done = True
             # break
 env.close()
-
